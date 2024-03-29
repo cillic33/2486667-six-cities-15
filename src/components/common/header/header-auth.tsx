335a48739d1d4e25ba7f7a0fb7117aa332ca57ed
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-03-29 19:44:48 +0700

    module8-task1 | Добавление иконки для ссылки Sign in

--- src/components/common/header/header-auth.tsx
+++ src/components/common/header/header-auth.tsx
@@ -17,8 +17,7 @@ export default function HeaderAuth() {
     <>
       <li className="header__nav-item user">
         <Link className="header__nav-link header__nav-link--profile" to={AppRoute.Favorites}>
-          <div className="header__avatar-wrapper user__avatar-wrapper">
-          </div>
+          <div className="header__avatar-wrapper user__avatar-wrapper"></div>
           <span className="header__user-name user__name">{user?.email}</span>
           <span className="header__favorite-count">{favoriteCount}</span>
         </Link>
--- src/components/common/header/header-no-auth.tsx
+++ src/components/common/header/header-no-auth.tsx
@@ -5,6 +5,7 @@ export default function HeaderNoAuth() {
   return (
     <li className="header__nav-item">
       <Link className="header__nav-link" to={AppRoute.Login}>
+        <div className="header__avatar-wrapper user__avatar-wrapper"></div>
         <span className="header__signout">Sign in</span>
       </Link>
     </li>
