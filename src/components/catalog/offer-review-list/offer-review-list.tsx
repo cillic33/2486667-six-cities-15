a6f17bda84ff4ca5164d29808ad2b944f1dac56f
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-04-02 13:44:22 +0700

    module8-task1 | Добавление memo

--- src/components/catalog/offer-review-list/offer-review-list.tsx
+++ src/components/catalog/offer-review-list/offer-review-list.tsx
@@ -2,7 +2,7 @@ import {Review} from '@/types/reviews';
 import OfferReviewItem from '@/components/catalog/offer-review-item/offer-review-item';
 import OfferReviewEmpty from '@/components/catalog/offer-review-empty/offer-review-empty';
 import {COUNT_VISIBLE_REVIEWS, HIDDEN_CLASS} from '@/components/catalog/offer-review-list/const';
-import {MouseEvent, useState} from 'react';
+import {memo, MouseEvent, useState} from 'react';
 import '@/components/catalog/offer-review-list/styles.css';
 import {clsx} from 'clsx';
 
@@ -10,7 +10,7 @@ type OfferReviewListProps = {
   reviews: Review[];
 }
 
-export default function OfferReviewList({ reviews }: OfferReviewListProps): JSX.Element {
+function OfferReviewList({ reviews }: OfferReviewListProps): JSX.Element {
   const sortedReviews = reviews.toSorted((a, b) => Number(new Date(b.date)) - Number(new Date(a.date)));
   const [currentVisibleReviews, setCurrentVisibleReviews] = useState<number>(0);
   const sortedVisibleReviews = sortedReviews.slice(0, COUNT_VISIBLE_REVIEWS + currentVisibleReviews);
@@ -42,3 +42,6 @@ export default function OfferReviewList({ reviews }: OfferReviewListProps): JSX.
     </>
   );
 }
+
+const MemoOfferReviewList = memo(OfferReviewList);
+export default MemoOfferReviewList;
