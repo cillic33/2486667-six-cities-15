52eabfcfa55ed9cad1bb7334e90333e5ad169d80
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-04-03 21:21:57 +0700

    module9-task1 | fix build bugs

--- src/types/favorites.ts
+++ src/types/favorites.ts
@@ -1,29 +1,27 @@
 import {OfferPreview} from '@/types/offer-preview';
 import {RequestStatus} from '@/utils/const';
 
-interface FavoritesState {
+export interface FavoritesState {
   favorites: OfferPreview[];
   requestStatus: RequestStatus;
   changeStatus: RequestStatus;
 }
 
-type Favorites = {
+export type Favorites = {
   [key: string]: OfferPreview[];
 }
 
-enum FavoriteStatus {
+export enum FavoriteStatus {
   Add = 1,
   Remove = 0,
 }
 
-type ChangeFavoriteArgs = {
+export type ChangeFavoriteArgs = {
   offerId: string;
   status: FavoriteStatus;
 }
 
-type ChangeFavoriteResponse = {
+export type ChangeFavoriteResponse = {
   offer: OfferPreview;
   status: FavoriteStatus;
 }
-
-export {FavoritesState, Favorites, FavoriteStatus, ChangeFavoriteArgs, ChangeFavoriteResponse};
