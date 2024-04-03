80ee4f23a10ffeee894e0593ccb0b7c311f42f7e
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-04-03 14:34:58 +0700

    module8-task1 | Тесты для Favorites slice

---
+++ src/store/slices/favorites.test.ts
@@ -0,0 +1,60 @@
+import {favoritesSlice} from '@/store/slices/favorites';
+import {RequestStatus} from '@/utils/const';
+import {fetchFavorites} from '@/store/thunks/favorites';
+import {makeFakeOfferCard} from '@/utils/mock';
+
+describe('Favorites slice', () => {
+  it('should return initial state with empty action', () => {
+    const emptyAction = { type: '' };
+    const expectedState = {
+      favorites: [],
+      requestStatus: RequestStatus.Idle,
+      changeStatus: RequestStatus.Idle,
+    };
+
+    const result = favoritesSlice.reducer(expectedState, emptyAction);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should return default initial state with empty action', () => {
+    const emptyAction = { type: '' };
+    const expectedState = {
+      favorites: [],
+      requestStatus: RequestStatus.Idle,
+      changeStatus: RequestStatus.Idle,
+    };
+
+    const result = favoritesSlice.reducer(undefined, emptyAction);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "requestStatus" to "RequestStatus.Loading" with "fetchFavorites.pending"', () => {
+    const expectedState = {
+      favorites: [],
+      requestStatus: RequestStatus.Loading,
+      changeStatus: RequestStatus.Idle,
+    };
+
+    const result = favoritesSlice.reducer(undefined, fetchFavorites.pending);
+
+    expect(result).toEqual(expectedState);
+  });
+
+  it('should set "favorites" to array with offer card, "requestStatus" to "RequestStatus.Success" with "fetchFavorites.fulfilled"', () => {
+    const mockOfferCard = makeFakeOfferCard();
+    const expectedState = {
+      favorites: [mockOfferCard],
+      requestStatus: RequestStatus.Success,
+      changeStatus: RequestStatus.Idle,
+    };
+
+    const result = favoritesSlice.reducer(
+      undefined,
+      fetchFavorites.fulfilled([mockOfferCard], '', undefined),
+    );
+
+    expect(result).toEqual(expectedState);
+  });
+});
---
+++ src/utils/mock.ts
@@ -0,0 +1,25 @@
+import {OfferPreview} from '@/types/offer-preview';
+
+export const makeFakeOfferCard = (): OfferPreview => ({
+  id: 'd9027e8b-affc-4dd5-8bfa-a99149c85785',
+  title: 'Tile House',
+  type: 'house',
+  price: 458,
+  previewImage: 'https://15.design.htmlacademy.pro/static/hotel/4.jpg',
+  city: {
+    name: 'Paris',
+    location: {
+      latitude: 48.85661,
+      longitude: 2.351499,
+      zoom: 13
+    }
+  },
+  location: {
+    latitude: 48.868610000000004,
+    longitude: 2.342499,
+    zoom: 16
+  },
+  isFavorite: true,
+  isPremium: true,
+  rating: 5
+} as OfferPreview);
