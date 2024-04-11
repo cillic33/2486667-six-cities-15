c688b1eb3b5404fddf13d9e700e0994397788dc2
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-04-12 00:12:30 +0700

    module9-task3 | Тест для OffersSort - fix linter

--- src/components/catalog/offers-list-empty/offers-list-empty.test.tsx
+++ src/components/catalog/offers-list-empty/offers-list-empty.test.tsx
@@ -2,7 +2,7 @@ import {describe, expect, it} from 'vitest';
 import {screen} from '@testing-library/react';
 import {renderWithRouterAndProviders} from '@/utils/mock-component';
 import {makeFakeCity} from '@/utils/mock';
-import OffersListEmpty from "@/components/catalog/offers-list-empty/offers-list-empty";
+import OffersListEmpty from '@/components/catalog/offers-list-empty/offers-list-empty';
 
 describe('Component: OffersListEmpty', () => {
   it('should render correctly', () => {
--- src/components/catalog/offers-sort/offers-sort.test.tsx
+++ src/components/catalog/offers-sort/offers-sort.test.tsx
@@ -1,7 +1,7 @@
 import {describe, expect, it} from 'vitest';
 import {screen} from '@testing-library/react';
 import {renderWithRouterAndProviders} from '@/utils/mock-component';
-import OffersSort from "@/components/catalog/offers-sort/offers-sort";
+import OffersSort from '@/components/catalog/offers-sort/offers-sort';
 
 describe('Component: OffersSort', () => {
   it('should render correctly', () => {
