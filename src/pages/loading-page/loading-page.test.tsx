f16677ee21ad88c76512b7eea25f29ee8de2fed2
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-04-11 16:12:44 +0700

    module9-task3 | fix loading page test

--- src/pages/loading-page/loading-page.test.tsx
+++ src/pages/loading-page/loading-page.test.tsx
@@ -1,14 +1,13 @@
-import LoadingPage from "@/pages/loading-page/loading-page";
 import { render, screen } from '@testing-library/react';
 import {describe, it, expect} from 'vitest';
+import MemoLoadingPage from "@/pages/loading-page/loading-page";
 
 describe('Component: Loading page', () => {
   it('should render correctly', () => {
     const loaderTestId = 'loader';
 
-    render(<LoadingPage />);
-    const loader = screen.getByTestId(loaderTestId);
+    render(<MemoLoadingPage />);
 
-    expect(loader).toBeInTheDocument();
+    expect(screen.getByTestId(loaderTestId)).toBeInTheDocument();
   });
 });
