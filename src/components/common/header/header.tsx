4a20f7997cab9969bf5fa7615f1dc10f3ee499a4
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-04-12 12:38:25 +0700

    module9-task3 | Тест для Header

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -2,16 +2,16 @@ import {useMatch} from 'react-router-dom';
 import {AppRoute} from '@/utils/const';
 import HeaderLogo from '@/components/common/header-logo/header-logo';
 import {useAuth} from '@/hooks/user-authorisation/user-authorisation';
-import HeaderAuth from '@/components/common/header/header-auth';
-import HeaderNoAuth from '@/components/common/header/header-no-auth';
 import {memo} from 'react';
+import HeaderAuth from '@/components/common/header-auth/header-auth';
+import HeaderNoAuth from '@/components/common/header-no-auth/header-no-auth';
 
 function Header(): JSX.Element {
   const isAuth = useAuth();
   const matchWithLoginPage = useMatch(AppRoute.Login);
 
   return (
-    <header className="header">
+    <header className="header" data-testid="header">
       <div className="container">
         <div className="header__wrapper">
           <div className="header__left">
