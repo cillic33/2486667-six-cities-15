3c8627ce68ee4240d8fcfe39953cfdf82797b5ac
Попова Маргарита Игоревна <mpopova@ibs.ru>
2024-04-12 12:39:55 +0700

    module9-task3 | fix - удаление memo

--- src/components/common/footer/footer.test.tsx
+++ src/components/common/footer/footer.test.tsx
@@ -1,13 +1,13 @@
 import {screen} from '@testing-library/react';
 import {describe, it, expect} from 'vitest';
 import {renderWithRouterAndProviders} from '@/utils/mock-component';
-import MemoFooter from '@/components/common/footer/footer';
+import Footer from '@/components/common/footer/footer';
 
 describe('Component: Footer', () => {
   it('should render correctly', () => {
     const footerTestId = 'footer';
 
-    renderWithRouterAndProviders(<MemoFooter />);
+    renderWithRouterAndProviders(<Footer />);
 
     expect(screen.getByTestId(footerTestId)).toBeInTheDocument();
   });
--- src/pages/loading-page/loading-page.test.tsx
+++ src/pages/loading-page/loading-page.test.tsx
@@ -1,12 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import {describe, it, expect} from 'vitest';
-import MemoLoadingPage from '@/pages/loading-page/loading-page';
+import LoadingPage from '@/pages/loading-page/loading-page';
 
 describe('Component: Loading page', () => {
   it('should render correctly', () => {
     const loaderTestId = 'loader';
 
-    render(<MemoLoadingPage />);
+    render(<LoadingPage />);
 
     expect(screen.getByTestId(loaderTestId)).toBeInTheDocument();
   });
